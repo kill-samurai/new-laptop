81155f2cb10fe252e696bf412dac4347b5a8cac1
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2025-04-15 18:38:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,5 +23,5 @@ brew install htop &&
 brew install --cask 1password &&
 brew install defaultbrowser &&
 defaultbrowser brave &&
-brew install dockutil
+brew install dockutil &&
 sudo curl -sL https://raw.githubusercontent.com/kill-samurai/nl/refs/heads/main/cont.sh | zsh
