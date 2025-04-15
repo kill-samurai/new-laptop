24e111f86781e6e7c9a8c2ba784529e6962e85b6
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2025-04-15 17:28:16 -0400

    Create cont.sh

---
+++ cont.sh
@@ -0,0 +1,13 @@
+brew install --cask postman && 
+brew install --cask brave-browser && 
+brew install node && 
+brew install --cask visual-studio-code && 
+brew install --cask dadroit-json-viewer && 
+brew install python@3.13 && 
+brew install --cask pycharm-ce && 
+brew install --cask firefox && 
+brew install --cask sublime-text && 
+brew install --cask spotify && 
+brew install --cask studio-3t && 
+brew install htop && 
+brew install --cask 1password
