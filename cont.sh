16475fff23f92251d9f8b9d0fa597f5651fc43de
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2025-04-15 18:21:28 -0400

    Update cont.sh

--- cont.sh
+++ cont.sh
@@ -1,15 +1,3 @@
-brew install --cask postman && 
-brew install --cask brave-browser && 
-brew install node && 
-brew install --cask visual-studio-code && 
-brew install --cask dadroit-json-viewer && 
-brew install python@3.13 && 
-brew install --cask pycharm-ce && 
-brew install --cask firefox && 
-brew install --cask sublime-text && 
-brew install --cask spotify && 
-brew install --cask studio-3t && 
-brew install htop && 
-brew install --cask 1password &&
-brew install defaultbrowser &&
-defaultbrowser brave
+defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false &&
+sudo defaults write /Library/Preferences/com.apple.AppleMultitouchTrackpad Clicking -bool true && 
+sudo defaults write /Library/Preferences/.GlobalPreferences com.apple.mouse.tapBehavior -int 1
