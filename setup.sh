b75e209f56941f10be6ef8a4a29641d4c772d61c
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2025-04-15 17:10:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,24 @@
-brew install --cask postman && brew install --cask brave-browser && brew install node && brew install --cask visual-studio-code && brew install --cask dadroit-json-viewer && brew install python@3.13 && brew install --cask pycharm-ce && brew install --cask firefox && brew install --cask sublime-text && brew install --cask spotify && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && brew install --cask studio-3t && brew install htop && softwareupdate --install-rosetta --agree-to-license && git clone https://github.com/ChesterYue/ohmyzsh-theme-passion && cp ./ohmyzsh-theme-passion/passion.zsh-theme ~/.oh-my-zsh/themes/passion.zsh-theme && brew install coreutils && curl -O https://raw.githubusercontent.com/kill-samurai/new-laptop/refs/heads/main/.zshrc
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+echo >> /Users/ll/.zprofile &&
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ll/.zprofile &&
+eval "$(/opt/homebrew/bin/brew shellenv)" &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+git clone https://github.com/ChesterYue/ohmyzsh-theme-passion &&
+cp ./ohmyzsh-theme-passion/passion.zsh-theme ~/.oh-my-zsh/themes/passion.zsh-theme &&
+brew install coreutils && 
+curl -O https://raw.githubusercontent.com/kill-samurai/new-laptop/refs/heads/main/.zshrc &&
+source .zshrc &&
+softwareupdate --install-rosetta --agree-to-license &&
+brew install --cask postman && 
+brew install --cask brave-browser && 
+brew install node && 
+brew install --cask visual-studio-code && 
+brew install --cask dadroit-json-viewer && 
+brew install python@3.13 && 
+brew install --cask pycharm-ce && 
+brew install --cask firefox && 
+brew install --cask sublime-text && 
+brew install --cask spotify && 
+brew install --cask studio-3t && 
+brew install htop && 
+brew install --cask 1password
