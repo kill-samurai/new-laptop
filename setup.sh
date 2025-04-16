7570684de72d6e5848f8b8e5a6554ec0f8800d72
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2025-04-15 22:43:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,22 @@ brew install --cask spotify &&
 brew install --cask studio-3t && 
 brew install htop && 
 brew install --cask 1password &&
+brew install --cask slack &&
 brew install defaultbrowser &&
 defaultbrowser chrome &&
 brew install dockutil &&
-curl -sL https://raw.githubusercontent.com/kill-samurai/nl/refs/heads/main/cont.sh | zsh
+defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false &&
+sudo defaults write /Library/Preferences/com.apple.AppleMultitouchTrackpad Clicking -bool true && 
+sudo defaults write /Library/Preferences/.GlobalPreferences com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.dock autohide -bool true &&
+defaults write com.apple.dock autohide-time-modifier -float 0.2 &&
+defaults write com.apple.dock tilesize -int 36 &&
+defaults write com.apple.dock magnification -bool true &&
+defaults write com.apple.dock largesize -int 64 &&
+defaults write com.apple.dock persistent-apps -array &&
+dockutil --add "/Applications/Google Chrome.app" && 
+dockutil --add /Applications/Utilities/Terminal.app &&
+dockutil --add /Applications/Visual\ Studio\ Code.app &&
+brew uninstall dockutil &&
+brew uninstall defaultbrowser &&
+sudo shutdown -r now
