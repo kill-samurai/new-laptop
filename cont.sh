07402bc9a31173b6c84b3118d2813e675e7ec599
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2025-04-15 22:30:57 -0400

    Update cont.sh

--- cont.sh
+++ cont.sh
@@ -1,14 +1,14 @@
-defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false &&
-defaults write /Library/Preferences/com.apple.AppleMultitouchTrackpad Clicking -bool true && 
-defaults write /Library/Preferences/.GlobalPreferences com.apple.mouse.tapBehavior -int 1
-defaults write com.apple.dock autohide -bool true &&
-defaults write com.apple.dock autohide-time-modifier -float 0.2 &&
-defaults write com.apple.dock tilesize -int 36 &&
-defaults write com.apple.dock magnification -bool true &&
-defaults write com.apple.dock largesize -int 64 &&
-defaults write com.apple.dock persistent-apps -array &&
-dockutil --add /Applications/Google\ Chrome.app && 
-dockutil --add /Applications/Utilities/Terminal.app &&
-dockutil --add /Applications/Visual\ Studio\ Code.app &&
-killall Dock &&
+sudo defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false &&
+sudo defaults write /Library/Preferences/com.apple.AppleMultitouchTrackpad Clicking -bool true && 
+sudo defaults write /Library/Preferences/.GlobalPreferences com.apple.mouse.tapBehavior -int 1
+sudo defaults write com.apple.dock autohide -bool true &&
+sudo defaults write com.apple.dock autohide-time-modifier -float 0.2 &&
+sudo defaults write com.apple.dock tilesize -int 36 &&
+sudo defaults write com.apple.dock magnification -bool true &&
+sudo defaults write com.apple.dock largesize -int 64 &&
+sudo defaults write com.apple.dock persistent-apps -array &&
+sudo dockutil --add /Applications/Google\ Chrome.app && 
+sudo dockutil --add /Applications/Utilities/Terminal.app &&
+sudo dockutil --add /Applications/Visual\ Studio\ Code.app &&
+sudo killall Dock &&
 curl -sL https://raw.githubusercontent.com/kill-samurai/nl/refs/heads/main/clean.sh | zsh
